055ac14c41e165d745fef7b2e4b436934e25ce95
prittk <114107523+prittk@users.noreply.github.com>
2023-10-09 18:10:01 -0400

    Update UserAssert.java

--- UserAssert.java
+++ UserAssert.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 //this program tests the assertion call to check if a number is between 0 and 10,
 // you must activate assertionto use the program, in ecliple its under the arguments tab in the 
 // VM arguments type -ea, or running from consol type -ea before the file java -ea UserAssert
+// https://www.geeksforgeeks.org/assertions-in-java/ resources used for assertion
 public class UserAssert {
 	
 	
